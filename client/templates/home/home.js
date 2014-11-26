a3af1e225e7d67a897733076b0c44c3875f0c9cd
Fulanke <franck@MacBook-Pro-de-Franck.local>
2014-11-26 18:04:23 +0100

    initial push

---
+++ client/templates/home/home.js
@@ -0,0 +1,39 @@
+Template.home.events(
+	{
+  'click .js-logout': function() {
+    Meteor.logout();
+		Router.go('signin');
+    
+    // if we are on a private list, we'll need to go to a public one
+    // var current = Router.current();
+ //    if (current.route.name === 'listsShow' && current.data().userId) {
+ //
+ //      Router.go('listsShow', Lists.findOne({userId: {$exists: false}}));
+ //    }
+  	},
+		'click #login-buttons-logout': function() {
+	    Meteor.logout();
+			Router.go('signin');
+		},
+		'click .en-lang': function() {
+			TAPi18n.setLanguage('en');
+		},
+		'click .fr-lang': function() {
+			TAPi18n.setLanguage('fr');
+		}
+	}
+);
+
+Template.home.helpers({
+  username: function () {
+     return Meteor.user() && Meteor.user().emails.shift().address;
+   },
+	 lang: function() {
+		 return TAPi18n.getLanguage();
+	 },
+	 langActiv: function(lang) {
+		 if(TAPi18n.getLanguage() === lang)
+			 return 'active';
+		 return '';
+	 }
+})
