8054f04405029c430200bd2d2f8732cdb8bf8f6f
Benjamin-Samuel Ewenczyk <ben.ewenczyk@gmail.com>
2014-12-11 17:08:21 +0100

    Display improvements

--- client/templates/observatory/observatory.js
+++ client/templates/observatory/observatory.js
@@ -27,29 +27,6 @@ Template.observatory.helpers({
                         ).fetch();
         }
     },
-    current_item: function(){
-        if ( Session.get('portfolio_level') !== undefined && Session.get('portfolio_level') ) {
-            //var curr_est_doc = Session.get('current_estate_doc');
-            var curr_est_doc = Estates.findOne(Session.get('current_estate_doc')._id)
-
-            // console.log(Portfolios.find().fetch().length);
-
-            // only return smthg if Session.get('current_estate_doc') has a value
-            if (curr_est_doc !== undefined && curr_est_doc.hasOwnProperty("portfolio_collection") ) {
-                console.log("I display current Portfolios");
-                var result = Portfolios.find({_id: {$in : curr_est_doc.portfolio_collection} },
-                        {sort: {name:1}}
-                        ).fetch();
-                console.log(result);
-                return result;
-            };
-        };
-        if ( Session.get('portfolio_level') !== undefined && !Session.get('portfolio_level') ) {
-            return Buildings.find({portfolio_id: Session.get('current_portfolio_doc')._id },
-                        {sort: {name:1}}
-                        ).fetch();
-        }
-    },
     displayName: function(){
         if ( Session.get('portfolio_level') !== undefined && Session.get('portfolio_level') ) {
             return this.name ;
@@ -72,15 +49,15 @@ Template.observatory.events({
     'click .select_portfolio': function() {
         Session.set('current_portfolio_doc', this); // "this" is passed by Meteor - it's the current item
 
-        // console.log("current Portfolio is: ");
-        // console.log(this);
+        // $( "#portfolio_list" ).fadeOut("fast", function() {
+        //     Session.set('portfolio_level', false);
+        //     $( "#building_list" ).fadeIn();
+        // });
 
-        //Session.set('portfolio_level', false);
-
-        $( "#portfolio_list" ).fadeOut("fast", function() {
+        if ( Session.get('portfolio_level') ) {
             Session.set('portfolio_level', false);
             $( "#building_list" ).fadeIn();
-        });
+        };
 
         // Meteor.subscribe('configurations', this._id);
         // console.log('INSERT - update_estate_var is now: ' +  Session.get('update_estate_var'));
