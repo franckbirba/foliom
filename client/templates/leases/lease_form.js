12cef6df51b7e61691f67e102c6f628fc7de8c15
Benjamin-Samuel Ewenczyk <ben.ewenczyk@gmail.com>
2015-01-02 13:22:00 +0100

    LeaseForm: conformity_information names

--- client/templates/leases/lease_form.js
+++ client/templates/leases/lease_form.js
@@ -31,113 +31,119 @@ AutoForm.hooks({
 });
 
 
-// $("[title^='Tom']")
-
-//<select name="technical_compliance.categories.0.conformity" required="" data-schema-key="technical_compliance.categories.0.conformity" autocomplete="off" class="form-control">
 
 Template.leaseForm.rendered = function () {
 
-        //Apply End-Use to correct field
-        var endUses = EndUse.find().fetch() ; // ToDo: check possible collision?
+    //Apply End-Use to correct field
+    var endUses = EndUse.find().fetch() ; // ToDo: check possible collision?
 
-        $(".end_use_name").each(function( index ) {
-            $(this).val( transr(endUses[index].end_use_name) );
-            $(this).prop("readonly","readonly") ;
-            // $(this).val( index );
+    $(".end_use_name").each(function( index ) {
+        $(this).val( transr(endUses[index].end_use_name) );
+        $(this).prop("readonly","readonly") ;
+        // $(this).val( index );
+    });
+
+    $(".technical_compliance_name").each(function( index ) {
+        $(this).val( transr( technical_compliance_items[index]) );
+        $(this).prop("readonly","readonly") ;
+        // $(this).val( index );
+    });
+
+
+
+    // Set values on change
+    $(".tcc_lifetime").change(function(){
+        $("[name='technical_compliance.global_lifetime']").val(
+            calc_qualitative_assessment_class(".tcc_lifetime")
+        )
+    });
+
+    $(".tcc_conformity").change(function(){
+        $("[name='technical_compliance.global_conformity']").val(
+            calc_qualitative_assessment_class(".tcc_conformity")
+        )
+    });
+
+    var totalHeatElecFluids = 0;
+    var totalHeatElecFluids_array = [];
+
+    // Monitor fluid_consumption_meter and apply formulas
+    Tracker.autorun(function () {
+
+        $("[name^='fluid_consumption_meter.'][name$='.yearly_cost']").each(function( index ) {
+        // "fluid_consumption_meter.0.yearly_cost"
+
+            var matchingFluid = AutoForm.getFieldValue("insertLeaseForm", "fluid_consumption_meter." + index + ".fluid_id") ;
+            var matchingYearlySubscription = AutoForm.getFieldValue("insertLeaseForm", "fluid_consumption_meter." + index + ".yearly_subscription") ;
+            var matchingFirstYearValue = AutoForm.getFieldValue("insertLeaseForm", "fluid_consumption_meter." + index + ".first_year_value") ;
+
+            if (matchingFluid) {
+
+                var correctFluid = fluidToObject(matchingFluid); // gets the Fluid obj in the conf. from a txt like "EDF - fluid_heat"
+
+                // console.log("correctFluid: ");
+                // console.log(correctFluid);
+
+                //target is for example fluid_consumption_meter.0.yearly_cost
+                $("[name='fluid_consumption_meter." + index + ".yearly_cost']").val(
+                    matchingYearlySubscription +
+                        matchingFirstYearValue * correctFluid.yearly_values[0].cost // cost et pas evolution_index
+                );
+
+                /* ---------------------------------------------- */
+                /* END-USE FORMULAS: consumption_by_end_use_total */
+                /* ---------------------------------------------- */
+                    // 1- Take advatage of this Loop to update totalHeatElecFluids
+                    if (curr_fluid_type == "fluid_electricity" || curr_fluid_type == "fluid_heat") {
+                        totalHeatElecFluids_array[index] = matchingFirstYearValue ;
+                    } else {
+                        totalHeatElecFluids_array[index] = 0;
+                    }
+
+                    // So now update the field
+                    totalHeatElecFluids = _.reduce(totalHeatElecFluids_array, function(memo, num){ return memo + num; }, 0);
+                    $("[name='consumption_by_end_use_total']").val( totalHeatElecFluids );
+            }
+
+            // ToDo: create 30 yearly Values
         });
+    });
 
-        $(".technical_compliance_name").each(function( index ) {
-            $(this).val( transr( technical_compliance_items[index]) );
-            $(this).prop("readonly","readonly") ;
-            // $(this).val( index );
+    /* ---------------- */
+    /* END-USE FORMULAS */
+    /* ---------------- */
+
+    // 1 - Set "Specific" field: always field #06
+    var endUseVal_array = [];
+
+    Tracker.autorun(function () {
+        $("[name^='consumption_by_end_use.'][name$='.first_year_value']").each(function( index ) {
+            // console.log(index);
+            if (index != 6) { // Exclude 6 as it's the specific field
+                var firstYearValue = AutoForm.getFieldValue("insertLeaseForm", "consumption_by_end_use." + index + ".first_year_value") ;
+                if(firstYearValue) {endUseVal_array[index] = firstYearValue ;}
+            } else {
+                endUseVal_array[index] = 0;
+            }
         });
+        console.log("endUseVal_array is: "+ endUseVal_array);
+        var specificFieldValue = _.reduce(endUseVal_array, function(memo, num){ return memo + num; }, 0);
+        var totalConsumption = AutoForm.getFieldValue("insertLeaseForm", "consumption_by_end_use_total") ;
+        $("[name='consumption_by_end_use.6.first_year_value']").val(
+            totalConsumption - specificFieldValue
+        ) ;
+    });
 
+    /* ------------------------------------------------------------------------ */
+    /* conformity_information (Conformité réglementaire / audits / diagnostics) */
+    /* ------------------------------------------------------------------------ */
 
-
-        // Set values on change
-        $(".tcc_lifetime").change(function(){
-            $("[name='technical_compliance.global_lifetime']").val(
-                calc_qualitative_assessment_class(".tcc_lifetime")
-            )
-        });
-
-        $(".tcc_conformity").change(function(){
-            $("[name='technical_compliance.global_conformity']").val(
-                calc_qualitative_assessment_class(".tcc_conformity")
-            )
-        });
-
-        var totalHeatElecFluids = 0;
-        var totalHeatElecFluids_array = [];
-
-        // Monitor fluid_consumption_meter and apply formulas
-        Tracker.autorun(function () {
-
-            $("[name^='fluid_consumption_meter.'][name$='.yearly_cost']").each(function( index ) {
-            // "fluid_consumption_meter.0.yearly_cost"
-
-                var matchingFluid = AutoForm.getFieldValue("insertLeaseForm", "fluid_consumption_meter." + index + ".fluid_id") ;
-                var matchingYearlySubscription = AutoForm.getFieldValue("insertLeaseForm", "fluid_consumption_meter." + index + ".yearly_subscription") ;
-                var matchingFirstYearValue = AutoForm.getFieldValue("insertLeaseForm", "fluid_consumption_meter." + index + ".first_year_value") ;
-
-                if (matchingFluid) {
-
-                    var correctFluid = fluidToObject(matchingFluid); // gets the Fluid obj in the conf. from a txt like "EDF - fluid_heat"
-
-                    // console.log("correctFluid: ");
-                    // console.log(correctFluid);
-
-                    //target is for example fluid_consumption_meter.0.yearly_cost
-                    $("[name='fluid_consumption_meter." + index + ".yearly_cost']").val(
-                        matchingYearlySubscription +
-                            matchingFirstYearValue * correctFluid.yearly_values[0].cost // cost et pas evolution_index
-                    );
-
-                    /* ---------------------------------------------- */
-                    /* END-USE FORMULAS: consumption_by_end_use_total */
-                    /* ---------------------------------------------- */
-                        // 1- Take advatage of this Loop to update totalHeatElecFluids
-                        if (curr_fluid_type == "fluid_electricity" || curr_fluid_type == "fluid_heat") {
-                            totalHeatElecFluids_array[index] = matchingFirstYearValue ;
-                        } else {
-                            totalHeatElecFluids_array[index] = 0;
-                        }
-
-                        // So now update the field
-                        totalHeatElecFluids = _.reduce(totalHeatElecFluids_array, function(memo, num){ return memo + num; }, 0);
-                        $("[name='consumption_by_end_use_total']").val( totalHeatElecFluids );
-                }
-
-                // ToDo: create 30 yearly Values
-            });
-        });
-
-        /* ---------------- */
-        /* END-USE FORMULAS */
-        /* ---------------- */
-
-        // 1 - Set "Specific" field: always field #06
-        var endUseVal_array = [];
-
-        Tracker.autorun(function () {
-            $("[name^='consumption_by_end_use.'][name$='.first_year_value']").each(function( index ) {
-                // console.log(index);
-                if (index != 6) { // Exclude 6 as it's the specific field
-                    var firstYearValue = AutoForm.getFieldValue("insertLeaseForm", "consumption_by_end_use." + index + ".first_year_value") ;
-                    if(firstYearValue) {endUseVal_array[index] = firstYearValue ;}
-                } else {
-                    endUseVal_array[index] = 0;
-                }
-            });
-            console.log("endUseVal_array is: "+ endUseVal_array);
-            var specificFieldValue = _.reduce(endUseVal_array, function(memo, num){ return memo + num; }, 0);
-            var totalConsumption = AutoForm.getFieldValue("insertLeaseForm", "consumption_by_end_use_total") ;
-            $("[name='consumption_by_end_use.6.first_year_value']").val(
-                totalConsumption - specificFieldValue
-            ) ;
-        });
-
-
+    // Set the Names
+    $("[name^='conformity_information.'][name$='.name']").each(function( index ) {
+        $(this).val( transr( conformity_information_items[index]) );
+        $(this).prop("readonly","readonly") ;
+        // $(this).val( index );
+    });
 };
 
 
