dc74d0b0675e67bbdf1ec51b785e5af7fab4afa7
PEM-- <pemarchandet@gmail.com>
2015-01-09 00:41:12 +0100

    Bootstrap: fonts, main layout, signin

--- client/templates/signin/signin.js
+++ client/templates/signin/signin.js
@@ -10,31 +10,37 @@ Template.signin.helpers({
   },
   errorClass: function(key) {
     return Session.get(ERRORS_KEY)[key] && 'error';
+  },
+  emailPlaceholder: function() {
+    return TAPi18n.__('your_email');
+  },
+  passwordPlaceholder: function() {
+    return TAPi18n.__('your_password');
   }
 });
 
 Template.signin.events({
   'submit': function(event, template) {
     event.preventDefault();
-    
+
     var email = template.$('[name=email]').val();
     var password = template.$('[name=password]').val();
-    
+
     var errors = {};
 
     if (! email) {
-      errors.email = 'Email is required';
+      errors.email = TAPi18n.__('email_required');
     }
 
     if (! password) {
-      errors.password = 'Password is required';
+      errors.password = TAPi18n.__('password_required');
     }
-    
+
     Session.set(ERRORS_KEY, errors);
     if (_.keys(errors).length) {
       return;
     }
-    
+
     Meteor.loginWithPassword(email, password, function(error) {
       if (error) {
         return Session.set(ERRORS_KEY, {'none': error.reason});
