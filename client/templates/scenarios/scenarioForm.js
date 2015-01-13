25dfaf325720a33c96e32fe0d502e91d74f456c8
Benjamin-Samuel Ewenczyk <ben.ewenczyk@gmail.com>
2015-01-13 03:15:22 +0100

    ScenarioForm: beginning of the screen (v3) - each loop for Criterion

--- client/templates/scenarios/scenarioForm.js
+++ client/templates/scenarios/scenarioForm.js
@@ -19,4 +19,12 @@ Template.scenarioForm.helpers({
     criterionName: function(){
         return TAPi18n.__("yearly_expense_max");
     },
+    getCriterion: function(){
+        return [
+            {"label": "Dépense annuelle max"},
+            {"label": "Consommation d’énergie en € / an : réduction au moins égale à"},
+            {"label": "Tenir compte de l’obsolescence pour déclencher les actions"},
+            {"label": "Priorité à l’indice global d’obsolescence"}
+            ];
+    },
 });
