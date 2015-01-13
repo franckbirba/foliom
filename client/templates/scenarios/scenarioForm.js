24348aa012f5836ac0a8c66479e060132c7dbd2a
Benjamin-Samuel Ewenczyk <ben.ewenczyk@gmail.com>
2015-01-13 20:55:50 +0100

    ScenarioForm-Criterion: basics for Selector and basics for removing Criterion

--- client/templates/scenarios/scenarioForm.js
+++ client/templates/scenarios/scenarioForm.js
@@ -13,18 +13,36 @@ Template.scenarioForm.helpers({
 Template.scenarioForm.rendered = function() {
     $( "#sortable" ).sortable();
     $( "#sortable" ).disableSelection();
+
+    //Remove item on click
+    $(".removeCriterion").click(function() {
+        // $(this).remove();
+        console.log( $(this).parents( ".criterion" )[0].remove() );
+    });
 };
 
 Template.scenarioForm.helpers({
     isCheckbox: function(type){
         return (type == "checkbox") ? true : false;
     },
+    isSelectorTechfield: function(type){
+        return (type == "selector_techfield") ? true : false;
+    },
     getCriterion: function(){
         return [
             {"label": "yearly_expense_max"},
             {"label": "energy_consum_atLeast_in_E_year"},
             {"label": "wait_for_obsolescence", "type":"checkbox", "desc": "wait_for_obsolescence_desc"},
-            {"label": "priority_to_gobal_obsolescence", "type":"checkbox", "desc": "priority_to_gobal_obsolescence_desc"}
+            {"label": "priority_to_gobal_obsolescence", "type":"checkbox", "desc": "priority_to_gobal_obsolescence_desc"},
+            {"label": "priority_to_techField", "type":"selector_techfield"}
             ];
     },
 });
+
+// Template.scenarioForm.events({
+//   'click .removeCriterion': function(event) {
+//     console.log(this);
+//     console.log(event.currentTarget);
+//     this.remove();
+//   },
+// });
