ddda80e6f9f306101d8368b34aa9f1486c29254d
Benjamin-Samuel Ewenczyk <ben.ewenczyk@gmail.com>
2015-01-21 15:31:29 +0100

    ScenarioForm: dynamic placeholders without helpers

--- client/templates/scenarios/scenarioForm.js
+++ client/templates/scenarios/scenarioForm.js
@@ -1,15 +1,7 @@
 //https://github.com/oorabona/reactive-table
 
 Template.scenarioForm.helpers({
-  name_pl: function() {
-    return TAPi18n.__('name');
-  },
-  duration_pl: function() {
-    return TAPi18n.__('In') + " " + TAPi18n.__('u_years');
-  },
-  total_expenditure_pl: function() {
-    return TAPi18n.__('In') + " " + TAPi18n.__('u_euros');
-  }
+
 });
 
 Template.scenarioForm.rendered = function() {
