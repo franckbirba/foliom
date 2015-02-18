76bbc106914995610b11dd29b00ee98fb0a29923
Benjamin-Samuel Ewenczyk <ben.ewenczyk@gmail.com>
2015-02-18 14:03:12 +0100

    Settings: simple and light array view for ICC

---
+++ app/lib/_utils/_settings_arrayBSE/_settings_arrayBSE.js
@@ -0,0 +1,17 @@
+if(Meteor.isClient){
+
+Template['quickForm_arraySettings'].helpers({
+  idPrefix: function () {
+    return this.atts["id-prefix"];
+  },
+  submitButtonAtts: function plQuickFormSubmitButtonAtts() {
+    var qfAtts = this.atts;
+    var atts = {};
+    if (typeof qfAtts.buttonClasses === "string") {
+      atts['class'] = qfAtts.buttonClasses;
+    }
+    return atts;
+  }
+});
+
+}
