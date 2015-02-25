af66fa9bca641f731d4733270d1e875914a2120f
Benjamin-Samuel Ewenczyk <ben.ewenczyk@gmail.com>
2015-02-25 10:27:45 +0100

    BuildingDetail: refactoring in coffeescript (continuing)

--- app/client/templates/buildings/buildingDetail/buildingDetail.coffee
+++ app/client/templates/buildings/buildingDetail/buildingDetail.coffee
@@ -1,9 +1,19 @@
+Template.buildingDetail.created = ->
+  instance = this
+  instance.waterFluids = new ReactiveVar([])
+
+  Session.set("current_lease_id", null) #Reset the var session associated to the Selector
+
+
 
 Template.buildingDetail.helpers
   getLeases: ->
     result = Leases.find({ building_id: Session.get('current_building_doc')._id }, sort: lease_name: 1).fetch()
     result
   waterConsumption: (param, precision) ->
+    waterFluids = Template.instance().waterFluids.get()
+    console.log waterFluids
+
     if waterFluids? #wait until the waterFluids array has been generated
 
       if Session.get('current_lease_id')?
@@ -34,6 +44,91 @@ Template.buildingDetail.helpers
           # return (correctWaterFluid.yearly_cost / correctWaterFluid.first_year_value).toFixed(precision);
           return 0
     return
+
+
+Template.buildingDetail.rendered = ->
+  current_building_doc_id = Session.get('current_building_doc')._id
+  allLeases = Leases.find(building_id: current_building_doc_id).fetch()
+
+  waterFluids = Template.instance().waterFluids.get()
+
+  #get the Water fluids for each Lease
+  _.each allLeases, (lease, i) ->
+    #For each lease, extract the fluid with the fluid_type to water
+    _.each lease.fluid_consumption_meter, (entry, i) ->
+      if entry.fluid_id.split(' - ')[1] is 'fluid_water'
+        # surcharge: add the surface and id to make the average easier
+        entry.surface = lease.area_by_usage
+        entry.lease_id = lease._id
+        waterFluids.push entry
+
+  console.log waterFluids
+  Template.instance().waterFluids.set(waterFluids)
+
+
+  ### ---------------------###
+  #  Create data for the Pie
+  ### ---------------------###
+
+  dataHolder = []
+  averagedData =
+    'text_domain': []
+    'data': []
+  totalSurface = 0
+  # Build the text domain and the Data
+  _.each allLeases, (entry, i) ->
+    dataHolder[i] = _id: entry._id
+    dataHolder[i].text_domain = entry.consumption_by_end_use.map((item) ->
+      item.end_use_name
+      # returns an array of the EndUse names
+    )
+    dataHolder[i].data = entry.consumption_by_end_use.map((item) ->
+      {
+        label: item.end_use_name
+        value: item.first_year_value
+      }
+    )
+    _.each dataHolder[i].data, (entry2, i) ->
+      # console.log("current entry is: ");
+      # console.log(entry2);
+      if _.where(averagedData.data, label: entry2.label).length == 0
+        # in this case the label does not exist
+        averagedData.text_domain.push entry2.label
+        averagedData.data.push
+          label: entry2.label
+          value: entry2.value * entry.area_by_usage
+      else
+        _.each averagedData.data, (entry3, i) ->
+          if entry3.label == entry2.label
+            # console.log("was heeere - label is: "+ entry2.label);
+            # console.log("entry3.value is: " + entry3.value);
+            # console.log("entry2.value (dataHolder value) is: " + entry2.value);
+            # console.log("area of useage is: " + entry.area_by_usage);
+            entry3.value += entry2.value * entry.area_by_usage
+          return
+      return
+    totalSurface += entry.area_by_usage
+    # console.log("dataHolder: "+i);
+    # console.log(JSON.stringify(dataHolder[i]) );
+    # console.log("averagedData: ");
+    # console.log(JSON.stringify(averagedData));
+    return
+  #And to finish: divide all data values by the Total surface
+  _.each averagedData.data, (dataItem, i) ->
+    dataItem.value = (dataItem.value / totalSurface).toFixed(2)
+    return
+  Session.set 'pieData', dataHolder
+  Session.set 'averagedPieData', averagedData
+
+  ### ------------------------------ ###
+
+  #Create data for the DPE barchart
+
+  ### ------------------------------ ###
+
+  #TODO
+  return
+
 
 
 Template.buildingDetail.events
--- app/client/templates/buildings/buildingDetail/buildingDetail.js
+++ app/client/templates/buildings/buildingDetail/buildingDetail.js
@@ -1,110 +1,107 @@
-Template.buildingDetail.created = function () {
-    waterFluids = [];
-};
+// Template.buildingDetail.created = function () {
+//     waterFluids = [];
+// };
 
-Template.buildingDetail.rendered = function () {
+// Template.buildingDetail.rendered = function () {
 
-    //Reset the var session associated to the Selector
-    Session.set("current_lease_id", null);
+//     var current_building_doc_id = Session.get('current_building_doc')._id;
+//     allLeases = Leases.find({building_id:current_building_doc_id}).fetch();
 
-    var current_building_doc_id = Session.get('current_building_doc')._id;
-    allLeases = Leases.find({building_id:current_building_doc_id}).fetch();
+//     /* ---------------------*/
+//     //get the Water fluids for each Lease
 
-    /* ---------------------*/
-    //get the Water fluids for each Lease
+//     _.each(allLeases, function(lease, i) {
+//         //For each lease, extract the fluid with the fluid_type to water
+//         _.each(lease.fluid_consumption_meter, function(entry, i) {
+//             if( entry.fluid_id.split(" - ")[1] == "fluid_water" ) {
+//                 // surcharge: add the surface and id to make the average easier
+//                 entry.surface = lease.area_by_usage;
+//                 entry.lease_id = lease._id ;
 
-    _.each(allLeases, function(lease, i) {
-        //For each lease, extract the fluid with the fluid_type to water
-        _.each(lease.fluid_consumption_meter, function(entry, i) {
-            if( entry.fluid_id.split(" - ")[1] == "fluid_water" ) {
-                // surcharge: add the surface and id to make the average easier
-                entry.surface = lease.area_by_usage;
-                entry.lease_id = lease._id ;
+//                 waterFluids.push(entry);
 
-                waterFluids.push(entry);
+//             }
+//         });
 
-            }
-        });
+//     });
+//     console.log(waterFluids);
 
-    });
-    console.log(waterFluids);
+//     /* ---------------------*/
+//     //Create data for the Pie
+//     /* ---------------------*/
 
-    /* ---------------------*/
-    //Create data for the Pie
-    /* ---------------------*/
-
-    var dataHolder = [];
-    var averagedData = {
-        "text_domain": [],
-        "data": []
-    };
-    var totalSurface = 0 ;
+//     var dataHolder = [];
+//     var averagedData = {
+//         "text_domain": [],
+//         "data": []
+//     };
+//     var totalSurface = 0 ;
 
 
-    // Build the text domain and the Data
-    _.each(allLeases, function(entry, i) {
-        dataHolder[i] = {
-            _id: entry._id
-        };
+//     // Build the text domain and the Data
+//     _.each(allLeases, function(entry, i) {
+//         dataHolder[i] = {
+//             _id: entry._id
+//         };
 
-        dataHolder[i].text_domain = entry.consumption_by_end_use.map(function(item){
-            return item.end_use_name; // returns an array of the EndUse names
-        });
+//         dataHolder[i].text_domain = entry.consumption_by_end_use.map(function(item){
+//             return item.end_use_name; // returns an array of the EndUse names
+//         });
 
-        dataHolder[i].data = entry.consumption_by_end_use.map(function(item){
-            return { label: item.end_use_name, value: item.first_year_value }
-        });
+//         dataHolder[i].data = entry.consumption_by_end_use.map(function(item){
+//             return { label: item.end_use_name, value: item.first_year_value }
+//         });
 
-        _.each(dataHolder[i].data, function(entry2, i) {
-            // console.log("current entry is: ");
-            // console.log(entry2);
+//         _.each(dataHolder[i].data, function(entry2, i) {
+//             // console.log("current entry is: ");
+//             // console.log(entry2);
 
-            if ( _.where(averagedData.data, {label: entry2.label}).length == 0 ) { // in this case the label does not exist
-                averagedData.text_domain.push( entry2.label );
+//             if ( _.where(averagedData.data, {label: entry2.label}).length == 0 ) { // in this case the label does not exist
+//                 averagedData.text_domain.push( entry2.label );
 
-                averagedData.data.push(
-                    { label: entry2.label, value: entry2.value*entry.area_by_usage }
-                );
-            }
-            else {
-                _.each(averagedData.data, function(entry3, i) {
-                    if (entry3.label == entry2.label) {
-                        // console.log("was heeere - label is: "+ entry2.label);
-                        // console.log("entry3.value is: " + entry3.value);
-                        // console.log("entry2.value (dataHolder value) is: " + entry2.value);
-                        // console.log("area of useage is: " + entry.area_by_usage);
+//                 averagedData.data.push(
+//                     { label: entry2.label, value: entry2.value*entry.area_by_usage }
+//                 );
+//             }
+//             else {
+//                 _.each(averagedData.data, function(entry3, i) {
+//                     if (entry3.label == entry2.label) {
+//                         // console.log("was heeere - label is: "+ entry2.label);
+//                         // console.log("entry3.value is: " + entry3.value);
+//                         // console.log("entry2.value (dataHolder value) is: " + entry2.value);
+//                         // console.log("area of useage is: " + entry.area_by_usage);
 
-                        entry3.value += entry2.value*entry.area_by_usage ;
-                    }
-                });
-            }
-        });
+//                         entry3.value += entry2.value*entry.area_by_usage ;
+//                     }
+//                 });
+//             }
+//         });
 
-        totalSurface += entry.area_by_usage;
+//         totalSurface += entry.area_by_usage;
 
-        // console.log("dataHolder: "+i);
-        // console.log(JSON.stringify(dataHolder[i]) );
-        // console.log("averagedData: ");
-        // console.log(JSON.stringify(averagedData));
+//         // console.log("dataHolder: "+i);
+//         // console.log(JSON.stringify(dataHolder[i]) );
+//         // console.log("averagedData: ");
+//         // console.log(JSON.stringify(averagedData));
 
-    });
+//     });
 
-    //And to finish: divide all data values by the Total surface
-    _.each(averagedData.data, function(dataItem, i) {
-        dataItem.value = (dataItem.value / totalSurface).toFixed(2) ;
-    });
+//     //And to finish: divide all data values by the Total surface
+//     _.each(averagedData.data, function(dataItem, i) {
+//         dataItem.value = (dataItem.value / totalSurface).toFixed(2) ;
+//     });
 
-    Session.set('pieData', dataHolder);
-    Session.set('averagedPieData', averagedData);
+//     Session.set('pieData', dataHolder);
+//     Session.set('averagedPieData', averagedData);
 
 
 
-    /* ------------------------------ */
-    //Create data for the DPE barchart
-    /* ------------------------------ */
-    //TODO
+//     /* ------------------------------ */
+//     //Create data for the DPE barchart
+//     /* ------------------------------ */
+//     //TODO
 
-};
+// };
 
 
 
