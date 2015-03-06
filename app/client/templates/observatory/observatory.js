f2da0d20f591e5361e14aaf1ae4a11506f60edad
Benjamin-Samuel Ewenczyk <ben.ewenczyk@gmail.com>
2015-03-06 17:12:38 +0100

    commit before sending import Docs to EGIS

--- app/client/templates/observatory/observatory.js
+++ app/client/templates/observatory/observatory.js
@@ -33,43 +33,44 @@ Template.observatory.events({
           encoding: "iso-8859-1",
           // newline: "\r",
           complete: function(results, file) {
-            // console.log("results.data are");
-            // console.log(results.data);
+            console.log("results.data are");
+            console.log(results.data);
 
-            _.each(results.data, function(element, index, list){
-              // console.log("[%d] - Portfolio: %s - Building: %s", index, element[0], element[1]);
+            // _.each(results.data, function(element, index, list){
+            //   // console.log("[%d] - Portfolio: %s - Building: %s", index, element[0], element[1]);
 
-              getGeocoding({street: element.address1, city: element.city, country: element.country},function(geo){
-                var tmpBuilding = {
-                  "building_name": element.building_name,
-                  "address": {
-                    "street": element.address1,
-                    "zip": element.zip_code,
-                    "city": element.city,
-                    "area": element.area,
-                    "country": element.country,
-                    "gps_lat": geo.gps_lat,
-                    "gps_long": geo.gps_long
-                  },
-                  "building_info": {
-                    "construction_year": element.construction_year,
-                    "building_control": element.building_control,
-                    "building_user": element.building_user,
-                    "area_total": element.area_total,
-                    "area_useful": element.area_useful,
-                    "building_nb_floors": element.building_nb_floors,
-                    "carpark_spaces": element.carpark_spaces,
-                    "carpark_area": element.carpark_area
-                  },
-                  "portfolio_id": Session.get('current_portfolio_doc')._id
-                };
-                 // console.log("tmpBuilding is");
-                 // console.log(tmpBuilding);
-              var newId = Buildings.insert(tmpBuilding);
-              // console.log('New building %s: %s created', tmpBuilding.building_name, newId);
-              });
+            //   getGeocoding({street: element.address1, city: element.city, country: element.country},function(geo){
+            //     var tmpBuilding = {
+            //       "building_name": element.building_name,
+            //       "address": {
+            //         "street": element.address1,
+            //         "zip": element.zip_code,
+            //         "city": element.city,
+            //         "area": element.area,
+            //         "country": element.country,
+            //         "gps_lat": geo.gps_lat,
+            //         "gps_long": geo.gps_long
+            //       },
+            //       "building_info": {
+            //         "construction_year": element.construction_year,
+            //         "building_control": element.building_control,
+            //         "building_user": element.building_user,
+            //         "area_total": element.area_total,
+            //         "area_useful": element.area_useful,
+            //         "building_nb_floors": element.building_nb_floors,
+            //         "carpark_spaces": element.carpark_spaces,
+            //         "carpark_area": element.carpark_area
+            //       },
+            //       "portfolio_id": Session.get('current_portfolio_doc')._id
+            //     };
+            //      // console.log("tmpBuilding is");
+            //      // console.log(tmpBuilding);
+            //   var newId = Buildings.insert(tmpBuilding);
+            //   // console.log('New building %s: %s created', tmpBuilding.building_name, newId);
+            //   });
 
-            });
+            // });
+
           }
         },
         complete: function() {
