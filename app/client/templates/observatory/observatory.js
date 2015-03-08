4a351b495a45f49cde39e61e61d6f4efdc2b77e9
Benjamin-Samuel Ewenczyk <ben.ewenczyk@gmail.com>
2015-03-08 18:58:44 +0100

    Moving import JS to buildingAndLeaseImport.js

--- app/client/templates/observatory/observatory.js
+++ app/client/templates/observatory/observatory.js
@@ -15,62 +15,6 @@ Template.observatory.helpers({
 
 
 Template.observatory.events({
-  'click .importPortfolio': function(){
-    // Excel files need to be saved as .txt UTF16 (see http://stackoverflow.com/questions/4221176/excel-to-csv-with-utf8-encoding)
-    $("#importPortfolio").parse({
-        config: {
-          delimiter: ";",
-          dynamicTyping: true,
-          header: true,
-          skipEmptyLines: true,
-          encoding: "iso-8859-1",
-          // newline: "\r",
-          complete: function(results, file) {
-            console.log("results.data are");
-            console.log(results.data);
-
-            // _.each(results.data, function(element, index, list){
-            //   // console.log("[%d] - Portfolio: %s - Building: %s", index, element[0], element[1]);
-
-            //   getGeocoding({street: element.address1, city: element.city, country: element.country},function(geo){
-            //     var tmpBuilding = {
-            //       "building_name": element.building_name,
-            //       "address": {
-            //         "street": element.address1,
-            //         "zip": element.zip_code,
-            //         "city": element.city,
-            //         "area": element.area,
-            //         "country": element.country,
-            //         "gps_lat": geo.gps_lat,
-            //         "gps_long": geo.gps_long
-            //       },
-            //       "building_info": {
-            //         "construction_year": element.construction_year,
-            //         "building_control": element.building_control,
-            //         "building_user": element.building_user,
-            //         "area_total": element.area_total,
-            //         "area_useful": element.area_useful,
-            //         "building_nb_floors": element.building_nb_floors,
-            //         "carpark_spaces": element.carpark_spaces,
-            //         "carpark_area": element.carpark_area
-            //       },
-            //       "portfolio_id": Session.get('current_portfolio_doc')._id
-            //     };
-            //      // console.log("tmpBuilding is");
-            //      // console.log(tmpBuilding);
-            //   var newId = Buildings.insert(tmpBuilding);
-            //   // console.log('New building %s: %s created', tmpBuilding.building_name, newId);
-            //   });
-
-            // });
-
-          }
-        },
-        complete: function() {
-          // console.log("All files done!");
-        }
-    });
-  },
   'click .select_portfolio': function() {
       Session.set('current_portfolio_doc', this); // "this" is passed by Meteor - it's the current item
 
@@ -94,30 +38,3 @@ Template.observatory.events({
       Router.go('building-detail', this);
   },
 });
-
-function getGeocoding(address, callback){
-  var tmpl = this;
-  VazcoMaps.init({}, function() {
-    var _geocoding = {gps_lat:0,gps_long:0};
-    tmpl.mapEngine = VazcoMaps.gMaps();
-    var tmp_address = address.street + " " + address.city + " " + address.country;
-    // console.log(tmp_address);
-
-    tmpl.mapEngine.geocode({
-      address: tmp_address,
-      callback: function(results, status) {
-        if (status == 'OK') {
-          var latlng = results[0].geometry.location;
-          // console.log(latlng);
-
-          _geocoding.gps_lat = latlng.lat();
-          _geocoding.gps_long = latlng.lng();
-
-          callback(_geocoding);
-
-        }
-      }
-    });
-
-  });
-}
