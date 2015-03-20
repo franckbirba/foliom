e543eece6a2d337986d91882032f62d580039ef6
Benjamin-Samuel Ewenczyk <ben.ewenczyk@gmail.com>
2015-03-20 04:03:22 +0100

    Optimizing calcs (done in created)

--- app/client/templates/dataViz/observatory_bars/observatoryBarchart.js
+++ app/client/templates/dataViz/observatory_bars/observatoryBarchart.js
@@ -14,16 +14,41 @@ Template.observatoryBarchart.events({
 
 Template.observatoryBarchart.created = function () {
   Session.set("observatoryBarchartDisplaySelector", "dpe_co2_emission" );
+  var instance = this ;
+  instance.barchartData = new ReactiveVar({})
+
+  // var data = [
+  //   {letter: "A", frequency: .08167},
+  //   {letter: "B", frequency: .04780},
+  // ];
+  // construction_year_data
+  instance.barchartData.construction_year_data = Buildings.find({},{sort: {name: 1}, fields: {building_name: 1, "building_info.construction_year" : 1} }).fetch().map(function(x){
+        return {letter:x.building_name, frequency: x.building_info.construction_year};
+      });
+
+  // ges Data
+  var dpe_co2_emission_data = Buildings.find({},{sort: {name: 1}, fields: {building_name: 1, "properties" : 1} }).fetch().map(function(x){
+        return {letter:x.properties.leases_averages.merged_dpe_ges_data.dpe_co2_emission.grade, frequency:x.building_name };
+      });
+  dpe_co2_emission_data = _.countBy(dpe_co2_emission_data, 'letter');
+  dpe_co2_emission_data = _.map(dpe_co2_emission_data,function(value, key){
+    return {letter:transr(key)(), frequency: value};
+    });
+  instance.barchartData.dpe_co2_emission_data = dpe_co2_emission_data;
+
+  instance.barchartData.set(instance.barchartData)
+
+
+  // var dpe_data = Buildings.find({},{sort: {name: 1}, fields: {building_name: 1, "properties" : 1} }).fetch();
 };
 
 Template.observatoryBarchart.rendered = function () {
   // var container_width = setTimeout(function() { $("#observatory_barchart_placeholder").width() }, 2000);
   var container_width = $("#observatory_barchart_placeholder").width();
   var data, display, y_legend, x_legend_rotation;
-
+  console.log("Template.currentData() is: ", Template.currentData() );
 
   this.autorun(function () {
-
     var margin = {top: 20, right: 20, bottom: 70, left: 40},
     width = container_width - margin.left - margin.right,
     height = 280 - margin.top - margin.bottom;
@@ -59,37 +84,18 @@ Template.observatoryBarchart.rendered = function () {
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
-    // OPTIMIZATION @BSE: CALC DATA IN CREATED
-    // var data = [
-    //   {letter: "A", frequency: .08167},
-    //   {letter: "B", frequency: .04780},
-    // ];
-    // construction_year_data
-    var construction_year_data = Buildings.find({},{sort: {name: 1}, fields: {building_name: 1, "building_info.construction_year" : 1} }).fetch().map(function(x){
-          return {letter:x.building_name, frequency: x.building_info.construction_year};
-        });
-
-    // ges Data
-    var dpe_co2_emission_data = Buildings.find({},{sort: {name: 1}, fields: {building_name: 1, "properties" : 1} }).fetch().map(function(x){
-          return {letter:x.properties.leases_averages.merged_dpe_ges_data.dpe_co2_emission.grade, frequency:x.building_name };
-        });
-    dpe_co2_emission_data = _.countBy(dpe_co2_emission_data, 'letter');
-    dpe_co2_emission_data = _.map(dpe_co2_emission_data,function(value, key){
-      return {letter:transr(key)(), frequency: value};
-      });
-
-    // var dpe_data = Buildings.find({},{sort: {name: 1}, fields: {building_name: 1, "properties" : 1} }).fetch();
-
 
     // Rules to display the correct data with the correct parameters
     display = Session.get("observatoryBarchartDisplaySelector");
 
+    var barchartData = Template.instance().barchartData.get() ;
+
     if (display == "construction_year") {
-      data = construction_year_data;
+      data = barchartData.construction_year_data;
       y_legend = 'construction_year';
       x_legend_rotation = "rotate(-45)";
     } else if (display == "dpe_co2_emission") {
-      data = dpe_co2_emission_data;
+      data = barchartData.dpe_co2_emission_data;
       y_legend = 'dpe_co2_emission';
       x_legend_rotation = "rotate(0)";
     }
