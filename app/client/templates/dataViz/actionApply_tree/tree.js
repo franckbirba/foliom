90e56a461ce2776b332b22782b5054a5e3b882ae
Benjamin-Samuel Ewenczyk <ben.ewenczyk@gmail.com>
2015-04-03 17:33:39 +0200

    Tree.js: fixing bug where checkboxes don't get selected on first click

--- app/client/templates/dataViz/actionApply_tree/tree.js
+++ app/client/templates/dataViz/actionApply_tree/tree.js
@@ -234,25 +234,28 @@ Template.treeTplt.rendered = function () {
                           building_name: d.name
                       });
 
-              // Set building session var
-              Session.set('current_building_doc', clickedBuilding);
+          // Set building session var
+          Session.set('current_building_doc', clickedBuilding);
 
-              // Set checkboxes
-              $(".ActionCheckbox").each(function () {
-                  // Look in child Actions if we find a match
-                  var actionID = $(this).val();
-                  actionExists = Actions.findOne({
-                                      "action_type":"child",
-                                      "building_id": Session.get('current_building_doc')._id,
-                                      "action_template_id": actionID
-                                      });
+          // Set checkboxes (with timeout, to give the Session time to propagate)
+          setTimeout(function(){
+            $(".ActionCheckbox").each(function () {
+                // Look in child Actions if we find a match
+                var actionID = $(this).val();
+                actionExists = Actions.findOne({
+                                    "action_type":"child",
+                                    "building_id": clickedBuilding._id,
+                                    "action_template_id": actionID
+                                    });
 
-                  if(actionExists) {
-                      $(this).prop("checked", true);
-                  } else {
-                      $(this).prop("checked", false);
-                  }
-              });
+                if(actionExists) {
+                    $(this).prop("checked", true);
+                } else {
+                    $(this).prop("checked", false);
+                }
+            });
+          }, 100);
+
         };
 
         // Case: building_to_actions > click on a building
