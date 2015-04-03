1664b9b7cce6451e422509819948380c14925c9e
Benjamin-Samuel Ewenczyk <ben.ewenczyk@gmail.com>
2015-04-03 17:47:46 +0200

    Getting click functions out of tree

--- app/client/templates/dataViz/actionApply_tree/tree.js
+++ app/client/templates/dataViz/actionApply_tree/tree.js
@@ -227,51 +227,6 @@ Template.treeTplt.rendered = function () {
         }
 
 
-        // Case: building_to_actions > click on a building
-        function building_to_actions_click_depth1(d){
-          var clickedBuilding = Buildings.findOne({
-                          portfolio_id: Session.get('current_portfolio_doc')._id,
-                          building_name: d.name
-                      });
-
-          // Set building session var
-          Session.set('current_building_doc', clickedBuilding);
-
-          // Set checkboxes (with timeout, to give the Session time to propagate)
-          setTimeout(function(){
-            $(".ActionCheckbox").each(function () {
-                // Look in child Actions if we find a match
-                var actionID = $(this).val();
-                actionExists = Actions.findOne({
-                                    "action_type":"child",
-                                    "building_id": clickedBuilding._id,
-                                    "action_template_id": actionID
-                                    });
-
-                if(actionExists) {
-                    $(this).prop("checked", true);
-                } else {
-                    $(this).prop("checked", false);
-                }
-            });
-          }, 100);
-
-        };
-
-        // Case: building_to_actions > click on a building
-        function building_to_actions_click_depth2(d){
-          // find the Action
-          actionExists = Actions.findOne({
-                              "action_type":"child",
-                              "building_id": Session.get('current_building_doc')._id,
-                              "name": d.name
-                              });
-
-          Session.set('childActionToEdit', actionExists);
-          Router.go('action-form');
-        };
-
-
         // Function to add functionnalities to wraptext
         function wraptext_wrapper(text){
           text.each(function(d) {
