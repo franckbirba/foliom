1a6b94ff3005ddb649ee6f79696323f52d3a90cc
Benjamin-Samuel Ewenczyk <ben.ewenczyk@gmail.com>
2015-04-19 17:32:02 +0200

    Cleanup in actionsList.js

--- app/client/templates/actions/actionsList/actionsList.js
+++ app/client/templates/actions/actionsList/actionsList.js
@@ -17,26 +17,7 @@ Template.actionsList.helpers({
        "estate_id": Session.get('current_estate_doc')._id,
        "action_type": "user_template"
     }).fetch();
-  },
-  getChildActions: function(){
-    return Actions.find({
-       "estate_id": Session.get('current_estate_doc')._id,
-       "action_type": "child"
-    }).fetch();
-  },
-  getActionLogo: function() {
-    // console.log($(this));
-    if(this.logo){
-      // Check if the img URL links to images in the public folder
-      if(this.logo.charAt(0) == "/") {
-          return this.logo;
-      }
-      else { return "/cfs/files/images/"+ this.logo; }
-    }
-    return "";
   }
-
-
 });
 
 
