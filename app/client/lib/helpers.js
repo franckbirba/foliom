d3fcd5b6d9855ec79b80fd1db3be2bcaf976ff18
Benjamin-Samuel Ewenczyk <ben.ewenczyk@gmail.com>
2015-05-28 02:22:07 +0200

    Roles: using helper from library

--- app/client/lib/helpers.js
+++ app/client/lib/helpers.js
@@ -23,15 +23,6 @@ Template.registerHelper('debugger', function() {
     debugger
 });
 
-// Check if User is Admin
-Template.registerHelper("isAdmin",
-    function(){
-        if(!Meteor.user() || !Meteor.user().roles)
-            return null;
-        return (Meteor.user().roles.indexOf('admin') >= 0 ? true : null);
-     }
-);
-
 // Get list of all Estates, sorted by alpha (on name)
 Template.registerHelper("estates",
     function(){
