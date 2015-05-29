d3f5c5109247a2a0eed35e9e852edef902c9a9b2
PEM-- <pemarchandet@gmail.com>
2015-05-29 18:06:45 +0200

    Meteor - Better import

--- docker/meteor/import.sh
+++ docker/meteor/import.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 rm -rf bundle app.tar.gz
-mkdir bundle
 cd ../../app
-meteor build --architecture os.linux.x86_64 ../docker/meteor/bundle/
+meteor build --architecture os.linux.x86_64 ../docker/meteor/
 cd -
-cd bundle
 tar xzvf app.tar.gz
+rm -rf app.tar.gz
