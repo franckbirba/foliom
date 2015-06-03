50d5a9903bc7cf575b2067283a6655c1990455a2
Benjamin-Samuel Ewenczyk <ben.ewenczyk@gmail.com>
2015-06-03 12:46:08 +0200

    refactoring remove helper

--- app/client/lib/helpers.js
+++ app/client/lib/helpers.js
@@ -1,143 +1,147 @@
 // Super "session" Helper
 Template.registerHelper('session', function(name) {
-    return Session.get(name);
+  return Session.get(name);
 });
 
 Template.registerHelper('session_equals', function(name, param) {
-    return Session.get(name) == param;
+  return Session.get(name) == param;
 });
 
 // Triple equals
 Template.registerHelper('isEqual', function(param1, param2) {
-    if (_.isArray(param1)) {
+  if (_.isArray(param1)) {
         // param1 is an array > check if the value exists inside the array
         return _.indexOf(param1, param2) > -1;
-    } else return param1 === param2;
-});
+      } else return param1 === param2;
+    });
 
 // Debugging Helper
 Template.registerHelper('console_log', function(param) {
-    console.log("param is", param);
+  console.log("param is", param);
 });
 Template.registerHelper('debugger', function() {
-    debugger
+  debugger
 });
 
 // Get list of all Estates, sorted by alpha (on name)
 Template.registerHelper("estates",
-    function(){
-        return Estates.find({},
-                    {sort: {estate_name:1}}
-                    ).fetch();
-    }
-);
+  function(){
+    return Estates.find({},
+      {sort: {estate_name:1}}
+      ).fetch();
+  }
+  );
 
 // Get list of all Portfolios
 Template.registerHelper("portfolios",
-    function(){
+  function(){
         // Pour l'instant on remonte tout
         return Portfolios.find({},
-                    {sort: {name:1}}
-                    ).fetch();
-    }
-);
+          {sort: {name:1}}
+          ).fetch();
+      }
+      );
 
 // Get list of all Portfolios for current Estate, sorted by alpha (on name)
 Template.registerHelper("getPortfolioList",
-    function(){
+  function(){
         // only return smthg if Session.get('current_estate_doc') has a value
         if (Session.get('current_estate_doc').hasOwnProperty("portfolio_collection") ) {
-            var result = Portfolios.find({_id: {$in : Session.get('current_estate_doc').portfolio_collection } },
-                    {sort: {name:1}}
-                    ).fetch();
-            return result;
+          var result = Portfolios.find({_id: {$in : Session.get('current_estate_doc').portfolio_collection } },
+            {sort: {name:1}}
+            ).fetch();
+          return result;
         };
-    }
-);
+      }
+      );
 
 Template.registerHelper("getBuildingList",
-    function(){
-        if ( Session.get('current_portfolio_doc') !== undefined ) {
-            return Buildings.find({portfolio_id: Session.get('current_portfolio_doc')._id },
-                        {sort: {building_name:1}}
-                        ).fetch();
-        }
+  function(){
+    if ( Session.get('current_portfolio_doc') !== undefined ) {
+      return Buildings.find({portfolio_id: Session.get('current_portfolio_doc')._id },
+        {sort: {building_name:1}}
+        ).fetch();
     }
-);
+  }
+  );
 
 // Return current estate_name
 Template.registerHelper("current_estate_name_H",
-    function(){
-        return Session.get('current_estate_doc')
-            ? Session.get('current_estate_doc').estate_name
-            : "ESTATE NOT DEFINED";
-    }
-);
+  function(){
+    return Session.get('current_estate_doc')
+    ? Session.get('current_estate_doc').estate_name
+    : "ESTATE NOT DEFINED";
+  }
+  );
 
 // Return current estate_name
 Template.registerHelper("current_portfolio_name_H",
-    function(){
-        return Session.get('current_portfolio_doc')
-            ? Session.get('current_portfolio_doc').name
-            : "PORTFOLIO NOT DEFINED";
-    }
-);
+  function(){
+    return Session.get('current_portfolio_doc')
+    ? Session.get('current_portfolio_doc').name
+    : "PORTFOLIO NOT DEFINED";
+  }
+  );
 
 Template.registerHelper("getActionsList",
-    function(){
+  function(){
         // ToDo : en cas d'update, il faut retirer l'Action en cours d'update. Sinon, cette même action va apparaître dans la liste (risque de référence circulaire)
         // var estate = Session.get('current_estate_doc');
         // Pour l'instant on remonte tout
         return Actions.find({},
-                    {sort: {name:1}}
-                    ).fetch();
-    }
-);
+          {sort: {name:1}}
+          ).fetch();
+      }
+      );
 
 Template.registerHelper("getBuildingName",
-    function(building_id){
-        if (building_id !== undefined && building_id !== null ) {
-            return Buildings.findOne(building_id).building_name;
-        }
+  function(building_id){
+    if (building_id !== undefined && building_id !== null ) {
+      return Buildings.findOne(building_id).building_name;
     }
-);
+  }
+  );
 
-Template.registerHelper("beforeRemove",
-    function(){
-        return function (collection, id, param) {
-            var doc = collection.findOne(id);
-            console.log("collection is", collection);
-            console.log("doc is", doc);
-            if( collection._name === "users" ){
-                if (confirm('Really delete "' + doc.profile.firstName + '"?')) {
-                  this.remove();
-                }
-            } else if( doc.hasOwnProperty('building_name') ){
-                if (confirm('Really delete "' + doc.building_name + '"?')) {
-                  this.remove();
-                }
-            } else if( doc.hasOwnProperty('lease_name') ){
-                if (confirm('Really delete "' + doc.lease_name + '"?')) {
-                  this.remove();
-                }
-            } else if( doc.hasOwnProperty('action_type') ){
-                if (confirm('Really delete "' + doc.name + '"?')) {
-                  this.remove();
-                }
-            } else if( doc.hasOwnProperty('duration') ){
-                if (confirm('Really delete "' + doc.name + '"?')) {
-                  this.remove();
-                }
-            } else if( collection._name === "selectors" ){
-                console.log(this.valueOf());
-                console.log(doc);
-                if (confirm('Really delete "' + doc.labels + '"?')) {
-                  // Meteor.call("updateSelector", doc.name, this.valueOf() );
-                }
-            } else {
-                console.log("collection is: ", collection);
-            }
-
-          };
+Template.registerHelper("beforeRemove", function(){
+  return function (collection, id, param) {
+    var doc = collection.findOne(id);
+    // console.log("collection is", collection);
+    // console.log("doc is", doc);
+    if( collection._name === "users" ){
+      if (confirm('Really delete "' + doc.profile.firstName + '"?')) {
+        this.remove();
+      }
+    } else if( collection._name === "buildings" ){
+      // Confirm that the Leases will also be deleted
+      if (confirm('Really delete "' + doc.building_name + '" and its associated usages?')) {
+        // Delete each Lease
+        _.each( Leases.find({'building_id': doc._id}, {fields: {'_id':1}}).fetch() , function(lease){
+          Leases.remove(lease._id);
+        });
+        // Delete Building
+        this.remove();
+      }
+    } else if( collection._name === "leases" ){
+      if (confirm('Really delete "' + doc.lease_name + '"?')) {
+        this.remove();
+      }
+    } else if( collection._name === "actions" ){
+      if (confirm('Really delete "' + doc.name + '"?')) {
+        this.remove();
+      }
+    } else if( collection._name === "scenarios" ){
+      if (confirm('Really delete "' + doc.name + '"?')) {
+        this.remove();
+      }
+    } else if( collection._name === "selectors" ){
+      console.log(this.valueOf());
+      console.log(doc);
+      if (confirm('Really delete "' + doc.labels + '"?')) {
+        // Meteor.call("updateSelector", doc.name, this.valueOf() );
+      }
+    } else {
+      console.log("collection is: ", collection);
     }
-);
+
+  };
+});
