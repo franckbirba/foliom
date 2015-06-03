65f5a7fe8a6f8bbe00cae7f51edfad1c262ff470
Benjamin-Samuel Ewenczyk <ben.ewenczyk@gmail.com>
2015-06-03 18:18:39 +0200

    users.js (form): cleanup

--- app/client/templates/user/user.js
+++ app/client/templates/user/user.js
@@ -53,50 +53,6 @@ Template.user.events({
   'submit': function(event, template) {
     event.preventDefault();
   }
-  // 'submit': function(event, template) {
-  //   event.preventDefault();
-  //   var email = template.$('[name=email]').val();
-  //    var username = template.$('[name=username]').val();
-  //   var password = template.$('[name=field_for_password]').val();
-  //   var confirm = template.$('[name=confirm]').val();
-  //   var roles = template.$('[name=roles]').val();
-  //   var errors = {};
-
-  //   if (! email) {
-  //     errors.email = 'Email required';
-  //   }
-
-  //   if (! username) {
-  //     errors.email = 'username required';
-  //   }
-
-  //   if (! password) {
-  //     errors.password = 'Password required';
-  //   }
-
-  //   if (confirm !== password) {
-  //     errors.confirm = 'Please confirm your password';
-  //   }
-
-  //   Session.set(ERRORS_KEY, errors);
-  //   if (_.keys(errors).length) {
-  //     return;
-  //   }
-
-  //   Meteor.call("addUser", {
-  //     profile:{username: username},
-  //     email: email,
-  //     password: password
-  //   }, function(error, result){
-  //     console.log(error, result);
-  //       if(roles){
-  //         Meteor.call("addRole", result, roles.split(','),function(error, result){
-  //           console.log(error, result);
-  //         });
-  //       }
-  //       template.$("#userform")[0].reset();
-  //   });
-  // }
 });
 
 AutoForm.hooks({
